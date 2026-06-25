669a436100f97c65572e19772dfdae74de07dbbd
Abdirakhman Asset <asetabdrakhman29052006@gmail.com>
2026-06-25 14:05:24 +0500

    feat: make imports and describe interfaces

--- frontend/src/app/components/review-dialog/review-dialog.ts
+++ frontend/src/app/components/review-dialog/review-dialog.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+
+
+export interface ReviewReceiver {
+  id: number;
+  username: string;
+}
+
+export interface ReviewResult {
+  rating: number;
+  comment: string;
+}
 
 @Component({
   selector: 'app-review-dialog',
@@ -6,6 +20,10 @@ import { Component } from '@angular/core';
   templateUrl: './review-dialog.html',
   styleUrl: './review-dialog.scss',
 })
+
+
+
+
 export class ReviewDialog {
 
 }
